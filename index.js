c03aef3e5f59fdaea58db2e682ab503bd742166d
Montse <montse.tellez.17@gmail.com>
2026-05-30 01:58:09 -0600

    Practica 1

---
+++ index.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+const PORT = process.env.PORT || 4000;
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+    console.log('Hola mundo en consola!!');
+    res.send('Servidor listo y corriendo!');
+});
+
+app.get('/alumnos/nombres', (req, res) => {
+    const alumnos = [
+        'Eduardo',
+        'Alberto',
+        'Diego',
+        'Alexis',
+        'Gael',
+        'Estrada'
+    ];
+
+    res.send(alumnos.join('<br>'));
+});
+
+app.listen(PORT, () => {
+    console.log(`Servidor escuchando en el puerto ${PORT}`);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
